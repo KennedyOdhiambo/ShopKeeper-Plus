4803aa4bc90d5d542b0b9fd55f2a1223f378032b
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-15 18:51:33 +0300

    improve login page responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { ModeToggle } from "@/components/ModeToggle"
 
 export default function Login() {
    return (
-      <main className="min-h-screen min-w-screen grid grid-cols-2">
+      <main className="min-h-screen min-w-screen grid md:grid-cols-2">
          <div className=" relative flex items-center">
-            <Image src={logo} alt="logo" className="absolute top-6 h-3.5 left-6" />
+            <Image src={logo} alt="logo" className="absolute top-6 ms-2 h-3.5 w-auto left-6" />
             <LoginCard />
          </div>
 
-         <div className="relative flex items-center">
+         <div className="hidden relative md:flex items-center">
             <div className="absolute top-6 right-6">
                <ModeToggle />
             </div>
