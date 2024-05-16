3f4fec9eaaf4d9e116b45c7c0812636058fd05e2
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-16 21:39:12 +0300

    begin working on sidebar

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import SignupForm from "../_features/authentication/SignupForm"
 
-export default function page() {
+export default function Signup() {
    return (
       <div className=" min-h-screen w-screen items-center justify-center flex">
          <SignupForm />
