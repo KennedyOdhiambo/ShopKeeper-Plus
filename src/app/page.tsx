ecfd7277fe49fdee2405cd48ddfb27d36e8acbef
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-18 14:08:29 +0300

    redirect user to auth as the default page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import Image from "next/image"
-import LoginCard from "./auth/components/LoginCard"
-import LoginCarousel from "./auth/components/LoginCarousel"
-import logo from "../../public/logo.svg"
-import { ModeToggle } from "@/components/ModeToggle"
+"use client"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
-export default function Login() {
-   return (
-      <main className="min-h-screen min-w-screen grid md:grid-cols-2">
-         <div className=" relative flex items-center">
-            <Image src={logo} alt="logo" className="absolute top-6 ms-2 h-3.5 w-auto left-6" />
-         </div>
+export default function Home() {
+   const router = useRouter()
 
-         <div className="hidden relative md:flex items-center">
-            <div className="absolute top-6 right-6">
-               <ModeToggle />
-            </div>
-            <LoginCarousel />
-         </div>
-      </main>
-   )
+   useEffect(() => {
+      router.push("/auth")
+   }, [router])
+
+   return null
 }
