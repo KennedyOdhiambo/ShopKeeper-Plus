8c8f9982823c02f9bce26b75cfe1d1dfd0b98ae2
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-18 15:14:32 +0300

    add navigation progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google"
 import "../styles/globals.css"
 import Providers from "@/context/Providers"
+import { ProgressBar } from "react-transition-progress"
 
 const poppins = Poppins({
    subsets: ["latin"],
@@ -15,7 +16,10 @@ export default function RootLayout({
    return (
       <html lang="en" suppressHydrationWarning>
          <body className={poppins.className}>
-            <Providers>{children}</Providers>
+            <Providers>
+               <ProgressBar className="fixed h-1 shadow-lg shadow-sky-500/20 bg-sky-500 top-0" />
+               {children}
+            </Providers>
          </body>
       </html>
    )
