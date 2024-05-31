17fa26309a8d2d11ec4332ec7ca3053ec4e5436b
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-31 07:09:43 +0300

    add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import maintenance from '../../public/maintenance.svg'
+import { Link } from 'react-transition-progress/next'
+
+export default function NotFound() {
+   return (
+      <div className="h-screen w-screen flex flex-col items-center px-5 justify-center">
+         <Image src={maintenance} alt="under construction" priority />
+         <p className="mx-auto py-5 text-center font-semibold">
+            Sorry, the page you requested for is still under maintenance, please check back soon!.
+            <Link href={'/user/dashboard'} className=" text-blue-500 ps-2 underline">
+               Back to dashboard
+            </Link>
+         </p>
+      </div>
+   )
+}
--- src/app/user/dashboard/components/RecentSalesSkeleton.tsx
+++ src/app/user/dashboard/components/RecentSalesSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export default function RecentSalesSkeleton() {
    return (
-      <div className=" space-y-8">
+      <div className=" space-y-6">
          {Array.from({ length: 5 }).map((_, i) => (
             <div key={i} className="flex items-center space-x-4">
                <Skeleton className="h-12 w-12 rounded-full" />
