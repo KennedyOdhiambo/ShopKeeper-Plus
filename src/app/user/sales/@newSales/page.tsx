e90bf8de33ce8844691f0180f33c73caa8b6093f
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-06-21 08:10:39 +0300

    succesfuly render customer list dropdown options

--- src/app/user/sales/@newSales/page.tsx
+++ src/app/user/sales/@newSales/page.tsx
@@ -1,10 +1,13 @@
+import { api } from '@/trpc/server';
 import NewSalesOptions from './_components/NewSalesOptions';
 import NewSalesTable from './_components/NewSalesTable';
 
-export default function NewSales() {
+export default async function NewSales() {
+   const customers = await api.customers.listCustomers({ userId: 'cba51dba-4308-453c-973a-0bb24c5fd6b4' });
+
    return (
       <div className="flex flex-col gap-4">
-         <NewSalesOptions />
+         <NewSalesOptions customers={customers} />
          <NewSalesTable />
       </div>
    );
